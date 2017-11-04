fb9a203b045ff37d80ad2f160bf268442ccaed2a
liuvigongzuoshi <liuvigongzuoshi@foxmail.com>
2017-11-04 23:30:53 +0800

    Add stage and img

---
+++ README.md
@@ -0,0 +1,5 @@
+#Gallery By React
+##使用ReactJS构架的图片画廊应用-学习React基础实战项目
+
+<!-- ## 许可
+[MIT](./LICENSE) &copy; -->
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,61 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
+// 获取图片相关的数据
+var imageDatas = require('../data/imageDatas.json');
 
-let yeomanImage = require('../images/yeoman.png');
+// 利用自执行函数， 将图片名信息转成图片URL路径信息
+imageDatas = (function genImageURL(imageDatasArr) {
+  for (var i = 0, j = imageDatasArr.length; i < j; i++) {
+    var singleImageData = imageDatasArr[i];
+
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+
+    imageDatasArr[i] = singleImageData;
+  }
+
+  return imageDatasArr;
+})(imageDatas);
+
+var ImgFigure = React.createClass({
+
+    render: function () {
+
+        return (
+            <figure className='img-figure'>
+                <img src={this.props.data.imageURL}
+                     alt={this.props.data.title}
+                />
+                <figcaption>
+                    <h2 className="img-title">{this.props.data.title}</h2>
+                </figcaption>
+            </figure>
+        );
+    }
+});
 
 class AppComponent extends React.Component {
   render() {
+     var controllerUnits = [],
+        imgFigures = [];
+        imageDatas.map(function (value,index) {
+        imgFigures.push(<ImgFigure data={value} key={index}/>);
+    });
+
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-        <span>hello world</span>
-      </div>
+      <section className='stage'>
+        <section className='img-sec'>
+            {imgFigures}
+         </section>
+         <nav className='controller-nav'>
+            {controllerUnits}
+        </nav>
+      </section>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
